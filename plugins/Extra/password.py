7ffb01a3a2d84dbbb6197120a9f7a2dd3fbb974c
SMK BOTZ <163844722+SMK-BOT@users.noreply.github.com>
2024-03-21 18:33:05 +0530

    Update password.py

--- plugins/Extra/password.py
+++ plugins/Extra/password.py
@@ -19,5 +19,5 @@ async def password(bot, update):
     limit = int(qw)
     random_value = "".join(random.sample(password, limit))
     txt = f"<b>Limit:</b> {str(limit)} \n<b>Password: <code>{random_value}</code>"
-    btn = InlineKeyboardMarkup([[InlineKeyboardButton('VJ Bots', url='https://t.me/vj_bots')]])
+    btn = InlineKeyboardMarkup([[InlineKeyboardButton('SMK THE BOTZ', url='https://t.me/SMKTHEBOTZ')]])
     await message.edit_text(text=txt, reply_markup=btn, parse_mode=enums.ParseMode.HTML)
